4d0d8462353b481969a098b33ea43698f7e0cc93
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-08-22 20:03:41 +1000

    Fixing unit test

--- src/test/java/guru/springframework/test/dstest/DataSourceTest.java
+++ src/test/java/guru/springframework/test/dstest/DataSourceTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(DataSourceConfig.class)
-@ActiveProfiles("guru")
+@ActiveProfiles("dev")
 public class DataSourceTest {
 
     private FakeDataSource fakeDataSource;
